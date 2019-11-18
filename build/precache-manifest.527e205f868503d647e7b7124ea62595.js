00f4d9ed53ed986cbcb4c3f5bd77d740f80c81ae
Adriana Castaneda <adriana@Adrianas-MBP.attlocal.net>
2019-11-18 12:47:58 -0600

    change config for heroku

---
+++ build/precache-manifest.527e205f868503d647e7b7124ea62595.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08cbe5b6d8ea7df47ccb096afa456b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f92831ca59df66f8920",
+    "url": "/static/js/2.befa6a2b.chunk.js"
+  },
+  {
+    "revision": "80041a4dbbf4def3b434",
+    "url": "/static/js/main.033fcf9b.chunk.js"
+  },
+  {
+    "revision": "64c1789360dc923e2bb4",
+    "url": "/static/js/runtime-main.59a505e6.js"
+  }
+]);
